a49ccf97374e6b57562f2e61d3cb3744165829b6
MalikMoulat <malik.moulat@hotmail.fr>
2022-05-19 13:54:30 +0200

    Add photographers data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +9,14 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city;
+        const p = document.createElement( 'p' );
+        p.textContent = tagline;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
         return (article);
     }
     return { name, picture, getUserCardDOM }
